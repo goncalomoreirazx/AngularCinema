a6dc51724d533b7bb4800554120e0de30006032c
Goncalo Moreira <goncalomoreiragit99@gmail.com>
2025-03-25 10:55:29 +0000

    New Navbar 25/03/2025 10:54

---
+++ src/app/pages/admin/admin-tickets/admin-tickets/admin-tickets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tickets',
+  imports: [],
+  templateUrl: './admin-tickets.component.html',
+  styleUrl: './admin-tickets.component.scss'
+})
+export class AdminTicketsComponent {
+
+}
