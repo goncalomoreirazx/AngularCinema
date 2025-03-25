0f7ab73fbfa8e6ff29334507691286aef3da4861
Goncalo Moreira <goncalomoreiragit99@gmail.com>
2025-03-25 12:32:22 +0000

    Tickets Managdment Admin 25/03/2025 12:32

--- src/app/pages/admin/admin-tickets/admin-tickets/admin-tickets.component.ts
+++ src/app/pages/admin/admin-tickets/admin-tickets/admin-tickets.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AdminTicketService, Ticket } from '../../../../services/admin-ticket-service';
+import { AdminTicketService, Ticket } from '../../../../services/admin-ticket.service';
 
 @Component({
   selector: 'app-admin-tickets',
